9a3a2dd126faf3ae790052fb633cf6fef27820e0
Edwino Stein <edwino.stein@gmail.com>
2019-10-22 14:45:42 -0400

    Refactor core functions to basic Arduino support

--- src/runtime/math.cpp
+++ src/runtime/math.cpp
@@ -7,24 +7,34 @@
 /* ******************************** Math Util ******************************** */
 
 long elrond::map(long x, long in_min, long in_max, long out_min, long out_max){
-    // Based in Arduino map funtion
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    #if defined GENERIC_STD_PLATFORM
+        // Based in Arduino map funtion
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    #elif defined INO_PLATFORM
+        return ::map(x, in_min, in_max, out_min, out_max);
+    #endif
 }
 
 int elrond::abs(int v){
     #if defined GENERIC_STD_PLATFORM
         return ::abs(v);
+    #elif defined INO_PLATFORM
+        return ((v) > 0 ? (v) : -(v));
     #endif
 }
 
 long int elrond::abs(long int v){
     #if defined GENERIC_STD_PLATFORM
         return ::abs(v);
+    #elif defined INO_PLATFORM
+        return ((v) > 0 ? (v) : -(v));
     #endif
 }
 
 double elrond::mod(double v){
     #if defined GENERIC_STD_PLATFORM
         return ::modf(v, nullptr);
+    #elif defined INO_PLATFORM
+        return modf(v, nullptr);
     #endif
 }
--- src/runtime/time.cpp
+++ src/runtime/time.cpp
@@ -11,6 +11,8 @@ unsigned long elrond::millis(){
     #if defined GENERIC_STD_PLATFORM
         auto duration = std::chrono::system_clock::now().time_since_epoch();
         return std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+    #elif defined INO_PLATFORM
+        return ::millis();
     #endif
 }
 
@@ -18,17 +20,23 @@ unsigned long elrond::micros(){
     #if defined GENERIC_STD_PLATFORM
         auto duration = std::chrono::system_clock::now().time_since_epoch();
         return std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
+    #elif defined INO_PLATFORM
+        return ::micros();
     #endif
 }
 
 void elrond::delay(unsigned long ms){
      #if defined GENERIC_STD_PLATFORM
         std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+    #elif defined INO_PLATFORM
+        ::delay(ms);
     #endif
 }
 
 void elrond::delayMicroseconds(unsigned int us){
      #if defined GENERIC_STD_PLATFORM
         std::this_thread::sleep_for(std::chrono::microseconds(us));
+    #elif defined INO_PLATFORM
+        ::delayMicroseconds(us);
     #endif
 }

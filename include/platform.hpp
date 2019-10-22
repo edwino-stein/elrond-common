fbc240fb5e286a506918491d7dba9dd3a1e74281
Edwino Stein <edwino.stein@gmail.com>
2019-10-22 14:32:05 -0400

    Add basic support to Arduino AVR boards

--- include/platform.hpp
+++ include/platform.hpp
@@ -30,6 +30,8 @@
         #define ELROND_GPIO_SERVO_TYPE ELROND_UINT16_TYPE
         #define ELROND_GPIO_PWM_TYPE ELROND_UINT16_TYPE
 
+        #define STR(S) S
+
         // Module Definition
         #define DEFINE_MODULE(CLASS)    elrond::interfaces::RuntimeInterface * elrond::__rtInstance__ = NULL;\
                                         extern "C" void _setAppInstance(elrond::interfaces::RuntimeInterface *app){elrond::__rtInstance__ = app;}\
@@ -43,6 +45,53 @@
         #define DEFINE_MODULE_AUTHOR_NAME(NAME) extern "C" const char *  _infoAuthorName(){return NAME;}
         #define DEFINE_MODULE_AUTHOR_EMAIL(EMAIL) extern "C" const char *  _infoAuthorEmail(){return EMAIL;}
         #define DEFINE_MODULE_VERSION(VERSION) extern "C" const char *  _infoVersion(){return VERSION;}
+
+    #elif defined ARDUINO_ARCH_AVR
+
+        // Includes
+        #include "Arduino.h"
+
+        #if defined ARDUINO_ARCH_AVR
+            #include "Servo.h"
+        #endif
+
+        // Defines
+        #define STR(S) F(S)
+        #define USE_LINKED_NODES
+        #define WITHOUT_DESTRUCTORS
+
+        // Generic macro for platforms that uses Arduino IDE
+        #define INO_PLATFORM
+
+        // Typedefs
+        #if defined ARDUINO_ARCH_AVR
+
+            #define ELROND_INT8_TYPE char
+            #define ELROND_UINT8_TYPE unsigned char
+            #define ELROND_INT16_TYPE int
+            #define ELROND_UINT16_TYPE unsigned int
+            #define ELROND_INT32_TYPE long
+            #define ELROND_UINT32_TYPE unsigned long
+
+            #define ELROND_GPIO_DIO_TYPE ELROND_UINT8_TYPE
+            #define ELROND_GPIO_AIO_TYPE ELROND_UINT16_TYPE
+            #define ELROND_GPIO_SERVO_TYPE Servo
+            #define ELROND_GPIO_PWM_TYPE ELROND_UINT16_TYPE
+
+        #endif
+
+        // Module Definition
+        #define DEFINE_MODULE(CLASS)
+        #define DEFINE_MODULE_PRETTY_NAME(NAME)
+        #define DEFINE_MODULE_AUTHOR_NAME(NAME)
+        #define DEFINE_MODULE_AUTHOR_EMAIL(EMAIL)
+        #define DEFINE_MODULE_VERSION(VERSION)
+
+        #undef highByte
+        #undef lowByte
+        #undef word
+        #undef INPUT
+        #undef abs
 
     #else
         #error "Incompatible Platform."

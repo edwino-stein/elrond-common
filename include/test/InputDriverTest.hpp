769be73c825180209de00d7671fbc22bb893c064
Edwino Stein <edwino.stein@gmail.com>
2020-01-21 13:52:15 -0400

    Move libelrond-test source files to include and src directories
    Issue #12

---
+++ include/test/InputDriverTest.hpp
@@ -0,0 +1,38 @@
+#if !defined  _ELROND_TEST_INPUT_DRIVER_HPP
+#define _ELROND_TEST_INPUT_DRIVER_HPP
+
+    #include "elrond.hpp"
+
+    #include <map>
+    #include <vector>
+    #include <memory>
+
+    namespace elrond {
+        namespace test {
+
+            class InputDriverTest : public elrond::modules::BaseInputDriverModule {
+
+                private:
+
+                    class InputCallbackTest: public elrond::input::InputCallback {
+                        public:
+                            InputCallbackTest(elrond::input::OnInputHandleT handle);
+                    };
+
+                    using InputCallbackTestP = std::unique_ptr<InputCallbackTest>;
+
+                protected:
+
+                    std::map<elrond::sizeT, elrond::input::InputCallback*> inputMap;
+                    std::vector<InputCallbackTestP> testInputCbInsts;
+
+                public:
+
+                    void addInputListener(const elrond::sizeT key, elrond::input::InputCallback *callback) override;
+                    void trigger(const elrond::sizeT key, const elrond::word data) const;
+                    void addInputListener(const elrond::sizeT key, elrond::input::OnInputHandleT handle);
+            };
+        }
+    }
+
+#endif

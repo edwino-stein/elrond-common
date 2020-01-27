d0dd42cd0422eb735bce695b9355ad894d68164e
Edwino Stein <edwino.stein@gmail.com>
2020-01-27 15:52:04 -0400

    Refactor elrond::interface::Module and runtime functions to inline functions support
    Issue #19

--- src/runtime/math.cpp
+++ src/runtime/math.cpp
@@ -4,18 +4,26 @@
     ******************* Elrond math functions implementation *******************
     ****************************************************************************/
 
-// Based in Arduino map funtion
-long elrond::map(const long x, const long inMin, const long inMax, const long outMin, const long outMax)
-{ return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin; }
+namespace elrond {
 
-int elrond::abs(int v)
-{ return ::abs(v); }
+    // Based in Arduino map funtion
+    ELROND_INLINE_FUNC long map(
+        const long x,
+        const long inMin,
+        const long inMax,
+        const long outMin,
+        const long outMax
+    ){ return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin; }
 
-long int elrond::abs(long int v)
-{ return ::abs(v); }
+    ELROND_INLINE_FUNC int abs(int v)
+    { return ::abs(v); }
 
-double elrond::mod(double v)
-{
-    double i;
-    return ::modf(v, &i);
+    ELROND_INLINE_FUNC long int abs(long int v)
+    { return ::abs(v); }
+
+    ELROND_INLINE_FUNC double mod(double v)
+    {
+        double i;
+        return ::modf(v, &i);
+    }
 }

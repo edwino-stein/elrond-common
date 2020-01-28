3a5e21bae9d1c7e55e9879396a00990cf73ff20b
Edwino Stein <edwino.stein@gmail.com>
2020-01-28 12:06:23 -0400

    Add external module test case
    Issue #18

---
+++ test/ExternalMod.cpp
@@ -0,0 +1,59 @@
+#include "elrond.hpp"
+
+class ExternalMod : public elrond::module::BaseModule {
+    protected:
+        elrond::channel::RxChannel rx;
+        elrond::channel::TxChannel tx;
+        elrond::gpio::DOutPin pin;
+        elrond::input::InputCallback input;
+
+    public:
+        virtual ~ExternalMod();
+        virtual void onInit(elrond::interface::ConfigMap& cfg, elrond::LoopControl& lc) override;
+        virtual void onStart() override;
+        virtual void loop() override;
+        virtual void onStop() override;
+};
+
+ExternalMod::~ExternalMod(){}
+
+void ExternalMod::onInit(elrond::interface::ConfigMap& cfg, elrond::LoopControl& lc)
+{
+    elrond::dout().putLn("External::onInit");
+
+    lc.enable = true;
+    lc.interval = 1000;
+
+    this->pin.attach(0);
+    this->rx.init(
+        0,
+        0,
+        [](const elrond::word data, elrond::TaskContext* const ctx)
+        { ((ExternalMod*) ctx)->pin.write(data); },
+        this
+    );
+
+    this->tx.init(0, 0);
+    this->input.init(
+        0,
+        0,
+        [](const elrond::word data, elrond::TaskContext* const ctx)
+        { ((ExternalMod*) ctx)->tx.trigger(data); },
+        this
+    );
+}
+
+void ExternalMod::onStart()
+{ elrond::dout().putLn("External::onStart"); }
+
+void ExternalMod::loop()
+{ elrond::dout().putLn("External::loop"); }
+
+void ExternalMod::onStop()
+{ elrond::dout().putLn("External::onStop"); }
+
+ELROND_DEFINE_MODULE(ExternalMod)
+ELROND_DEFINE_MODULE_PRETTY_NAME("External Module Test")
+ELROND_DEFINE_MODULE_AUTHOR_NAME("Elrond Half-elven")
+ELROND_DEFINE_MODULE_AUTHOR_EMAIL("elrond@rivendell.com")
+ELROND_DEFINE_MODULE_VERSION(ELROND_TO_STR_CONCAT(ELROND_API_VERSION.ELROND_API_REVISION-ELROND_API_DEVSTATE))
--- test/RuntimeTest.test.cpp
+++ test/RuntimeTest.test.cpp
@@ -1,14 +1,19 @@
 #include "elrond-test.hpp"
 #include "catch.hpp"
 
-#include <string>
-
 using elrond::test::RuntimeTest;
+using elrond::test::ExternalModuleTest;
+using elrond::test::TransportTest;
+using elrond::test::ChannelManagerTest;
+using elrond::test::InputDriverTest;
+using elrond::test::GpioTest;
 using elrond::test::DebugOut;
 using elrond::test::ConfigMap;
 
 using elrond::module::Example;
 using elrond::LoopControl;
+using elrond::gpio::BaseGpioPin;
+using elrond::gpio::DOutPin;
 
 TEST_CASE("Runtime application for Elrond Test Library")
 {
@@ -42,4 +47,63 @@ TEST_CASE("Runtime application for Elrond Test Library")
                [&loops](){ return loops++ < 1; }
             );
     }());
+}
+
+TEST_CASE("External module Test")
+{
+    DebugOut dout([](std::ostringstream& oss){ UNSCOPED_INFO(oss.str()); });
+    TransportTest transport;
+    ChannelManagerTest chm(transport, 2);
+    InputDriverTest input;
+    GpioTest gpio(
+        [&gpio, &ats](BaseGpioPin& pin, elrond::word data){
+            CHECK(pin.getType() == elrond::GpioType::DOUT);
+            CHECK(data == HIGH_VALUE);
+            gpio.write((DOutPin&) pin, data);
+        }
+    );
+
+    RuntimeTest appt;
+
+    RuntimeTest::setAppInstance(&appt);
+    appt.set(dout)
+        .set(chm)
+        .set(input)
+        .set(gpio);
+
+    REQUIRE_NOTHROW(
+        [&appt, &input, &ats]()
+        {
+            ExternalModuleTest inst("build/test/external-module.so", appt);
+
+            CHECK(inst.apiVer == elrond::makeDWord(ELROND_API_VERSION, ELROND_API_REVISION));
+            CHECK(inst.className == "ExternalMod");
+            CHECK(inst.prettyName == "External Module Test");
+            CHECK(inst.authorName == "Elrond Half-elven");
+            CHECK(inst.authorEmail == "elrond@rivendell.com");
+            CHECK(inst.version == ELROND_TO_STR_CONCAT(ELROND_API_VERSION.ELROND_API_REVISION-ELROND_API_DEVSTATE));
+
+            ConfigMap cfg;
+            LoopControl lc;
+            int loops = 0;
+
+            appt.init(inst.getInstance(), cfg, lc);
+
+            CHECK(lc.enable == true);
+            CHECK(lc.ownThread == false);
+            CHECK(lc.interval == 1000);
+
+            appt.start(
+               inst.getInstance(),
+               lc,
+               [&loops, &input]()
+               {
+                   if(loops++ >= 1) return false;
+                   input.trigger(0, HIGH_VALUE);
+                   return true;
+               }
+            );
+
+        }()
+    );
 }

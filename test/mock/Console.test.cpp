1f1200ad1fa425ecb1ca607243351b28df772bb9
Edwino Stein <edwino.stein@gmail.com>
2021-11-21 18:22:42 -0400

    Refactor mock::Console and mock::StringStream tests to BDD-style

--- test/mock/Console.test.cpp
+++ test/mock/Console.test.cpp
@@ -5,81 +5,136 @@ using elrond::mock::Console;
 using elrond::mock::StringStream;
 using elrond::interface::Stream;
 
-TEST_CASE("[elrond::mock::Console] Check stream handle info")
+SCENARIO("Test a mocked console instance for info outputs", "[mock][Console]")
 {
-    StringStream ss;
+    GIVEN("A Console and StringStram instance setted to handle info outputs")
+    {
+        StringStream ss;
+        Console console([&ss](const elrond::StreamH& h) { h(ss); });
 
-    Console console(
-        [&ss](const elrond::StreamH& handle)
+        WHEN("Calls info method passing a single char")
         {
-            ss.flush();
-            handle(ss);
+            console.info('a');
+            THEN("The StringStream must capture the same single char")
+            {
+                CHECK(ss.getString() == "a");
+            }
         }
-    );
 
-    console.info([](Stream& s) { s << "Hello world"; });
-    CHECK(ss.getString() == "Hello world");
-
-    console.info([](Stream& s) { s << 123; });
-    CHECK(ss.getString() == "123");
-
-    console.info([](Stream& s) { s << 3.14; });
-    CHECK(ss.getString() == "3.14");
-
-    elrond::string str = "Hello World";
-    console.info([&str](Stream& s) { s << "Message" << ':' << str << "!!!"; });
-    CHECK(ss.getString() == "Message:Hello World!!!");
-}
-
-TEST_CASE("[elrond::mock::Console] Check info prints")
-{
-    StringStream ss;
-
-    Console console(
-        [&ss](const elrond::StreamH& handle)
+        WHEN("Calls info method passing a single unsigned char")
         {
-            ss.flush();
-            handle(ss);
+            console.info(static_cast<unsigned char>(255));
+            THEN("The StringStream must capture the same single unsigned char")
+            {
+                CHECK(
+                    static_cast<unsigned char>(ss.getString()[0]) ==
+                    static_cast<unsigned char>(255)
+                );
+            }
         }
-    );
 
-    console.info(123);
-    CHECK(ss.getString() == "123");
+        WHEN("Calls info method passing a literal string")
+        {
+            console.info("Hello World literal"); 
+            THEN("The StringStream must capture the same literal string")
+            {
+                CHECK(ss.getString() == "Hello World literal");
+            }
+        }
 
-    console.info(static_cast<unsigned int>(-1));
-    CHECK(ss.getString() == "4294967295");
+        WHEN("Calls info method passing a const char* string")
+        {
+            const char* strConstChar = "Hello world const char*";
+            console.info(strConstChar);
+            THEN("The StringStream must capture the same const char* string")
+            {
+                CHECK(ss.getString() == "Hello world const char*");
+            }
+        }
 
-    console.info(static_cast<long>(0xFFFFFFFFFFFFFFFF));
-    CHECK(ss.getString() == "-1");
+        WHEN("Calls info method passing an elrond string")
+        {
+            elrond::string strElrondString = "Hello world elrond string";
+            console.info(strElrondString);
+            THEN("The StringStream must capture the same elrond string")
+            {
+                CHECK(ss.getString() == "Hello world elrond string");
+            }
+        }
 
-    console.info(static_cast<unsigned long>(0xFFFFFFFFFFFFFFFF));
-    CHECK(ss.getString() == "18446744073709551615");
+        WHEN("Calls info method passing an integer")
+        {
+            console.info(123);
+            THEN("The StringStream must capture the integer as string")
+            {
+                CHECK(ss.getString() == "123");
+            }
+        }
 
-    console.info(3.14159265359);
-    CHECK(ss.getString() == "3.14159");
+        WHEN("Calls info method passing an unsigned integer")
+        {
+            console.info(static_cast<unsigned int>(-1));
+            THEN("The StringStream must capture the unsigned integer as string")
+            {
+                CHECK(ss.getString() == "4294967295");
+            }
+        }
 
-    console.info(static_cast<float>(3.14159265359));
-    CHECK(ss.getString() == "3.14159");
+        WHEN("Calls info method passing a long")
+        {
+            console.info(static_cast<long>(0xFFFFFFFFFFFFFFFF));
+            THEN("The StringStream must capture the long as string")
+            {
+                CHECK(ss.getString() == "-1");
+            }
+        }
 
-    console.info('a');
-    CHECK(ss.getString() == "a");
+        WHEN("Calls info method passing an unsigned long")
+        {
+            console.info(static_cast<unsigned long>(0xFFFFFFFFFFFFFFFF));
+            THEN("The StringStream must capture the unsigned long as string")
+            {
+                CHECK(ss.getString() == "18446744073709551615");
+            }
+        }
 
-    console.info(static_cast<unsigned char>(255));
-    CHECK(static_cast<unsigned char>(ss.getString()[0]) == static_cast<unsigned char>(255));
+        WHEN("Calls info method passing a double")
+        {
+            console.info(3.14159265359);
+            THEN("The StringStream must capture the double as string")
+            {
+                CHECK(ss.getString() == "3.14159");
+            }
+        }
 
-    console.info("Hello World literal");
-    CHECK(ss.getString() == "Hello World literal");
+        WHEN("Calls info method passing a float")
+        {
+            console.info(static_cast<float>(3.14159265359));
+            THEN("The StringStream must capture the float as string")
+            {
+                CHECK(ss.getString() == "3.14159");
+            }
+        }
 
-    const char* strConstChar = "Hello world const char*";
-    console.info(strConstChar);
-    CHECK(ss.getString() == "Hello world const char*");
+        WHEN("Calls info method passing a lambda function")
+        {
+            elrond::string str = "Hello World";
+            console.info(
+                [&str](Stream& s)
+                {
+                    s << "Message" << ':' << str << "!!!\n";
+                    s << "Integer:" << 123 << '\n';
+                    s << "Double:" << 3.14159265359 << '\n';
+                }
+            );
 
-
-    char strCharArray[] = "Hello world char array";
-    console.info(strCharArray);
-    CHECK(ss.getString() == "Hello world char array");
-
-    elrond::string strElrondString = "Hello world elrond string";
-    console.info(strElrondString);
-    CHECK(ss.getString() == "Hello world elrond string");
+            THEN("The StringStream must capture everything as string")
+            {
+                CHECK(
+                    ss.getString() ==
+                    "Message:Hello World!!!\nInteger:123\nDouble:3.14159\n"
+                );
+            }
+        }
+    }
 }
--- test/mock/StringStream.test.cpp
+++ test/mock/StringStream.test.cpp
@@ -3,145 +3,297 @@
 
 using elrond::mock::StringStream;
 
-TEST_CASE("[elrond::mock::StringStream] Check integers print")
+SCENARIO("Test a mocked StringStream for capture numeric outputs", "[mock][StringStream]")
 {
-    StringStream ss;
+    GIVEN("A StringStram instance")
+    {
+        StringStream ss;
 
-    ss << 123;
-    CHECK(ss.getString() == "123");
+        WHEN("Capture a positive integer")
+        {
+            ss << 123;
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "123");
+            }
+        }
 
-    ss.flush();
-    ss << -123;
-    CHECK(ss.getString() == "-123");
+        WHEN("Capture a negative integer")
+        {
+            ss << -123;
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "-123");
+            }
+        }
 
-    ss.flush();
-    ss << static_cast<int>(0xFFFFFFFF);
-    CHECK(ss.getString() == "-1");
+        WHEN("Capture an overflowed integer")
+        {
+            ss << static_cast<int>(0xFFFFFFFF);
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "-1");
+            }
+        }
 
-    ss.flush();
-    ss << static_cast<unsigned int>(-1);
-    CHECK(ss.getString() == "4294967295");
+        WHEN("Capture an unsigned integer")
+        {
+            ss << static_cast<unsigned int>(0xFFFFFFFF);
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "4294967295");
+            }
+        }
 
-    ss.flush();
-    ss << static_cast<long>(0xFFFFFFFF);
-    CHECK(ss.getString() == "4294967295");
+        WHEN("Capture a negative integer as unsigned integer")
+        {
+            ss << static_cast<unsigned int>(-1);
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "4294967295");
+            }
+        }
 
-    ss.flush();
-    ss << static_cast<long>(0xFFFFFFFFFFFFFFFF);
-    CHECK(ss.getString() == "-1");
+        WHEN("Capture a long")
+        {
+            ss << static_cast<long>(0xFFFFFFFF);
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "4294967295");
+            }
+        }
 
-    ss.flush();
-    ss << static_cast<unsigned long>(0xFFFFFFFFFFFFFFFF);
-    CHECK(ss.getString() == "18446744073709551615");
+        WHEN("Capture an overflowed long")
+        {
+            ss << static_cast<long>(0xFFFFFFFFFFFFFFFF);
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "-1");
+            }
+        }
 
-    ss.flush();
-    ss << static_cast<unsigned long>(-1);
-    CHECK(ss.getString() == "18446744073709551615");
+        WHEN("Capture an unsigned long")
+        {
+            ss << static_cast<unsigned long>(0xFFFFFFFFFFFFFFFF);
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "18446744073709551615");
+            }
+        }
+
+        WHEN("Capture a negative long as unsigned long")
+        {
+            ss << static_cast<unsigned long>(-1);
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "18446744073709551615");
+            }
+        }
+
+        WHEN("Capture a positive double")
+        {
+            ss << 123.456;
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "123.456");
+            }
+        }
+
+        WHEN("Capture a negative double")
+        {
+            ss << -123.456;
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "-123.456");
+            }
+        }
+
+        WHEN("Capture a positive float")
+        {
+            ss << static_cast<float>(123.456);
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "123.456");
+            }
+        }
+
+        WHEN("Capture a negative float")
+        {
+            ss << static_cast<float>(-123.456);
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "-123.456");
+            }
+        }
+
+        WHEN("Capture a double with precision loss")
+        {
+            ss << 3.14159265359;
+            THEN("The StringStream must captured the number as string")
+            {
+                CHECK(ss.getString() == "3.14159");
+            }
+        }
+    }
 }
 
-TEST_CASE("[elrond::mock::StringStream] Check double/float print")
+SCENARIO("Test a mocked StringStream for capture string and char outputs", "[mock][StringStream]")
 {
-    StringStream ss;
+    GIVEN("A StringStram instance")
+    {
+        StringStream ss;
 
-    ss << 123.456;
-    CHECK(ss.getString() == "123.456");
+        WHEN("Capture a single printable char")
+        {
+            ss << 'a';
+            THEN("The StringStream must captured the symbol as string")
+            {
+                CHECK(ss.getString() == "a");
+            }
+        }
 
-    ss.flush();
-    ss << -123.456;
-    CHECK(ss.getString() == "-123.456");
+        WHEN("Capture a single printable char from your ASCII code")
+        {
+            ss << static_cast<char>(97);
+            THEN("The StringStream must captured the symbol as string")
+            {
+                CHECK(ss.getString() == "a");
+            }
+        }
 
-    ss.flush();
-    ss << static_cast<float>(123.456);
-    CHECK(ss.getString() == "123.456");
+        WHEN("Capture a single unprintable char from your ASCII code")
+        {
+            ss << static_cast<char>(127);
+            THEN("The StringStream must captured the symbol as string")
+            {
+                CHECK(ss.getString()[0] == static_cast<char>(127));
+            }
+        }
 
-    ss.flush();
-    ss << static_cast<float>(-123.456);
-    CHECK(ss.getString() == "-123.456");
+        WHEN("Capture a single unprintable char from your ASCII negative code")
+        {
+            ss << static_cast<char>(-1);
+            THEN("The StringStream must captured the symbol as string")
+            {
+                CHECK(ss.getString()[0] == static_cast<char>(-1));
+            }
+        }
 
-    ss.flush();
-    ss << 3.14159265359;
-    CHECK(ss.getString() == "3.14159");
+        WHEN("Capture a single printable const char from your ASCII code")
+        {
+            ss << static_cast<char>(97);
+            THEN("The StringStream must captured the symbol as string")
+            {
+                CHECK(ss.getString() == "a");
+            }
+        }
 
-    ss.flush();
-    ss << static_cast<double>(3.14159265359);
-    CHECK(ss.getString() == "3.14159");
+        WHEN("Capture a single unprintable const char from your ASCII code")
+        {
+            ss << static_cast<char>(255);
+            THEN("The StringStream must captured the symbol as string")
+            {
+                CHECK(
+                    static_cast<unsigned char>(ss.getString()[0]) ==
+                    static_cast<unsigned char>(255)
+                );
+            }
+        }
 
-    ss.flush();
-    ss << static_cast<float>(3.14159265359);
-    CHECK(ss.getString() == "3.14159");
+        WHEN("Capture a single unprintable const char from your ASCII negative code")
+        {
+            ss << static_cast<char>(-1);
+            THEN("The StringStream must captured the symbol as string")
+            {
+                CHECK(
+                    static_cast<unsigned char>(ss.getString()[0]) ==
+                    static_cast<unsigned char>(-1)
+                );
+            }
+        }
+
+        WHEN("Capture a literal string")
+        {
+            ss << "Hello world literal";
+            THEN("The StringStream must captured the string")
+            {
+                CHECK(ss.getString() == "Hello world literal");
+            }
+        }
+
+        WHEN("Capture a const char string")
+        {
+            const char* strConstChar = "Hello world const char";
+            ss << strConstChar;
+            THEN("The StringStream must captured the string")
+            {
+                CHECK(ss.getString() == "Hello world const char");
+            }
+        }
+
+        WHEN("Capture a const char array")
+        {
+            char strCharArray[] = "Hello world char array";
+            ss << strCharArray;
+            THEN("The StringStream must captured the string")
+            {
+                CHECK(ss.getString() == "Hello world char array");
+            }
+        }
+
+        WHEN("Capture an elrond string")
+        {
+            elrond::string strElrondString = "Hello world elrond string";
+            ss << strElrondString;
+            THEN("The StringStream must captured the string")
+            {
+                CHECK(ss.getString() == "Hello world elrond string");
+            }
+        }
+    }
 }
 
-TEST_CASE("[elrond::mock::StringStream] Check char print")
+SCENARIO("Test a mocked StringStream for capture concatened outputs", "[mock][StringStream]")
 {
-    StringStream ss;
+    GIVEN("A StringStram instance")
+    {
+        StringStream ss;
 
-    ss << 'a';
-    CHECK(ss.getString() == "a");
+        WHEN("Capture a string and chars")
+        {
+            ss << "Hello" << ' ' << "world";
+            THEN("The StringStream must captured the concated string")
+            {
+                CHECK(ss.getString() == "Hello world");
+            }
+        }
 
-    ss.flush();
-    ss << 'A';
-    CHECK(ss.getString() == "A");
-
-    ss.flush();
-    ss << static_cast<char>(97);
-    CHECK(ss.getString() == "a");
-
-    ss.flush();
-    ss << static_cast<char>(127);
-    CHECK(ss.getString()[0] == static_cast<char>(127));
-
-    ss.flush();
-    ss << static_cast<char>(-1);
-    CHECK(ss.getString()[0] == static_cast<char>(-1));
-
-    ss.flush();
-    ss << static_cast<unsigned char>(97);
-    CHECK(ss.getString() == "a");
-
-    ss.flush();
-    ss << static_cast<unsigned char>(255);
-    CHECK(static_cast<unsigned char>(ss.getString()[0]) == static_cast<unsigned char>(255));
-
-    ss.flush();
-    ss << static_cast<unsigned char>(-1);
-    CHECK(static_cast<unsigned char>(ss.getString()[0]) == static_cast<unsigned char>(-1));
+        WHEN("Capture a string and numbers")
+        {
+            ss << "average = (" << "50" << '+' << 86 << '+' << 37.5 << ")/" << 3;
+            THEN("The StringStream must captured the concated string")
+            {
+                CHECK(ss.getString() == "average = (50+86+37.5)/3");
+            }
+        }
+    }
 }
 
-TEST_CASE("[elrond::mock::StringStream] Check string print")
+SCENARIO("Test a mocked StringStream cleaning buffer", "[mock][StringStream]")
 {
-    StringStream ss;
+    GIVEN("A StringStram instance with something captured")
+    {
+        StringStream ss;
+        ss << "Hello" << ' ' << "world" << ' ' << 123;
+        REQUIRE(ss.getString() == "Hello world 123");
 
-    ss << "Hello world literal";
-    CHECK(ss.getString() == "Hello world literal");
-
-    ss.flush();
-    const char * strConstChar= "Hello world const char*";
-    ss << strConstChar;
-    CHECK(ss.getString() == "Hello world const char*");
-
-    ss.flush();
-    char strCharArray[] = "Hello world char array";
-    ss << strCharArray;
-    CHECK(ss.getString() == "Hello world char array");
-
-    ss.flush();
-    elrond::string strElrondString = "Hello world elrond string";
-    ss << strElrondString;
-    CHECK(ss.getString() == "Hello world elrond string");
-}
-
-TEST_CASE("[elrond::mock::StringStream] Check concatenation print")
-{
-    StringStream ss;
-
-    ss << "Hello" << ' ' << "world";
-    CHECK(ss.getString() == "Hello world");
-
-    ss.flush();
-    ss << "Pi = " << 3.14159265359 << ';';
-    CHECK(ss.getString() == "Pi = 3.14159;");
-
-    ss.flush();
-    ss << "average = (" << "50" << '+' << 86 << '+' << "37.5" << ")/" << 3;
-    CHECK(ss.getString() == "average = (50+86+37.5)/3");
+        WHEN("Call flush method")
+        {
+            ss.flush();
+            THEN("The captured string must be gone")
+            {
+                CHECK(ss.getString() == "");
+            }
+        }
+    }
 }

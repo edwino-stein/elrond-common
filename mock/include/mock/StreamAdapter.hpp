1ad6e07436d12d190b9c5ff4c5dbbde426fe7fe0
Edwino Stein <edwino.stein@gmail.com>
2023-04-12 15:29:26 -0400

    [MOCK] Add mock::StreamAdapter class

---
+++ mock/include/mock/StreamAdapter.hpp
@@ -0,0 +1,37 @@
+#ifndef ELROND_MOCK_STREAM_ADAPTER_HPP
+    #define ELROND_MOCK_STREAM_ADAPTER_HPP
+
+    #include "elrond_mock_types.hpp"
+
+    namespace elrond
+    {
+        namespace mock
+        {
+            class StreamAdapter : public elrond::interface::StreamAdapter
+            {
+                public:
+                    using AppendH = elrond::procedure<const elrond::interface::Stream&>;
+
+                private:
+                    elrond::interface::Stream& _stream;
+                    AppendH _preAppend;
+                    AppendH _postAppend;
+
+                public:
+                    StreamAdapter(elrond::interface::Stream& stream, AppendH preAppend, AppendH postAppend);
+
+                    const elrond::interface::Stream& stream() const override;
+                    void preAppend() const override;
+                    void postAppend() const override;
+
+                    static void eolAppend(const elrond::interface::Stream& stream);
+                    static void voidAppend(const elrond::interface::Stream& stream);
+
+                    static elrond::pointer<elrond::interface::StreamAdapter> makePretty(elrond::interface::Stream& stream, elrond::string tag);
+                    static elrond::pointer<elrond::interface::StreamAdapter> makeNull(elrond::interface::Stream& stream, elrond::string tag);
+            };
+
+        }
+    }
+
+#endif
---
+++ test/mock/StreamAdapter.test.cpp
@@ -0,0 +1,119 @@
+#include <elrond_mock.hpp>
+#include "catch2.hpp"
+
+using elrond::mock::StreamAdapter;
+using elrond::runtime::OStream;
+using elrond::interface::Stream;
+using Catch::Matchers::Contains;
+using Catch::Matchers::Matches;
+
+SCENARIO("Test mocked StreamAdapter instances", "[mock][StreamAdapter]")
+{
+    std::ostringstream oss;
+    OStream stream(oss);
+
+    GIVEN("A custom instance of StreamAdapter")
+    {
+        StreamAdapter adapter(
+            stream,
+            [](const Stream& s) { s << "PRE-APPEND"; },
+            [](const Stream& s) { s << "POST-APPEND"; }
+        );
+
+        WHEN("Call info stream getter")
+        {
+            auto& s = adapter.stream();
+            THEN("The returned info stream object must be the same object passed to adapter constructor")
+            {
+                CHECK(&s == &stream);
+            }
+        }
+
+        WHEN("Call pre append operation")
+        {
+            adapter.preAppend();
+            THEN("The string in StringStream should equal to 'PRE-APPEND'")
+            {
+                CHECK(oss.str() == "PRE-APPEND");
+            }
+        }
+
+        WHEN("Call post append operation")
+        {
+            adapter.postAppend();
+            THEN("The string in StringStream should equal to 'POST-APPEND'")
+            {
+                CHECK(oss.str() == "POST-APPEND");
+            }
+        }
+    }
+
+    GIVEN("An instance made by StreamAdapter::makePretty()")
+    {
+        auto adapter = StreamAdapter::makePretty(stream, "TEST");
+
+        WHEN("Call info stream getter")
+        {
+            auto& s = adapter->stream();
+            THEN("The returned info stream object must be the same object passed to adapter constructor")
+            {
+                CHECK(&s == &stream);
+            }
+        }
+
+        WHEN("Call pre append operation")
+        {
+            adapter->preAppend();
+            THEN("The string in StringStream should contains the same tag string")
+            {
+                CHECK_THAT(oss.str(), Contains("TEST"));
+            }
+
+            AND_THEN("The string capured in StringStream should be a string with expected pattern")
+            {
+                CHECK_THAT(oss.str(), Matches("\\[.*\\]\\t"));
+            }
+        }
+
+        WHEN("Call post append operation")
+        {
+            adapter->postAppend();
+            THEN("The string capured in StringStream should be a string EOL symbol")
+            {
+                CHECK(oss.str() == "\n");
+            }
+        }
+    }
+
+    GIVEN("An instance made by StreamAdapter::makeNull()")
+    {
+        auto adapter = StreamAdapter::makeNull(stream, "TEST");
+
+        WHEN("Call info stream getter")
+        {
+            auto& s = adapter->stream();
+            THEN("The returned info stream object must be the same object passed to adapter constructor")
+            {
+                CHECK(&s == &stream);
+            }
+        }
+
+        WHEN("Call pre append operation")
+        {
+            adapter->preAppend();
+            THEN("The string capured in StringStream should be an empty string")
+            {
+                CHECK(oss.str() == "");
+            }
+        }
+
+        WHEN("Call post append operation")
+        {
+            adapter->postAppend();
+            THEN("The string capured in StringStream should be an empty string")
+            {
+                CHECK(oss.str() == "");
+            }
+        }
+    }
+}
